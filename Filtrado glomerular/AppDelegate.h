09a4b86aeddadb5cb9c73b104d9b450442b8e1e4
Jose <josemsg@gmail.com>
2018-04-25 15:03:51 +0200

    Initial Commit

---
+++ Filtrado glomerular/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Filtrado glomerular
+//
+//  Created by Jose on 25/4/18.
+//  Copyright © 2018 Copanonga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Filtrado glomerular/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Filtrado glomerular
+//
+//  Created by Jose on 25/4/18.
+//  Copyright © 2018 Copanonga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
